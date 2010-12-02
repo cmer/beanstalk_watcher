fe60cdd0b3a11fe48d19a89a763d86ce323cfebf
Carl Mercier <carl@carlmercier.com>
2010-12-02 17:41:56 -0500

    Can now list tubes

--- beanstalk_watcher.rb
+++ beanstalk_watcher.rb
@@ -6,7 +6,7 @@ DEFAULT_BEANSTALK_PORT = 11300
 
 get '/' do
   out = ""
-  ["/beanstalk_jobs_ready", "/beanstalk_tube_stats"].each do |path|
+  ["/beanstalk_tubes", "/beanstalk_jobs_ready", "/beanstalk_tube_stats"].each do |path|
     out += "<a href='#{path}'>#{path}</a><br />"
   end
   out
@@ -19,7 +19,7 @@ get '/beanstalk_jobs_ready' do
   end
 
   host, fail_threshold, tube, port, raw = params[:host], params[:fail_threshold], params[:tube], params[:port] || DEFAULT_BEANSTALK_PORT, (params[:raw].to_s.to_bool)
-  
+
   # Query beanstalkd
   begin
     stats = beanstalk_stats(host, port, tube)
@@ -32,10 +32,10 @@ get '/beanstalk_jobs_ready' do
   end
 
   jobs_ready = stats["current-jobs-ready"]
-      
+
   over_threshold = fail_threshold != nil && jobs_ready >= fail_threshold.to_i
   status(500) if over_threshold
-  
+
   if raw
     return "#{jobs_ready}"
   else
@@ -51,7 +51,7 @@ get '/beanstalk_tube_stats' do
     status 404
     return "You must provide the following values: host, tube. Optionally, you can also provide: port."
   end
-  
+
   host, tube, port = params[:host], params[:tube], params[:port] || DEFAULT_BEANSTALK_PORT
 
   begin
@@ -69,6 +69,26 @@ get '/beanstalk_tube_stats' do
   end
 end
 
+get '/beanstalk_tubes' do
+  if params[:host].nil?
+    status 404
+    return "You must provide the following values: host, tube. Optionally, you can also provide: port."
+  end
+  host, tube, port = params[:host], params[:tube], params[:port] || DEFAULT_BEANSTALK_PORT
+
+  conn = beanstalk_connection(host, port)
+  tubes = conn.list_tubes.values[0]
+
+  out = "<font face='arial' size='+2'><ul>"
+
+  tubes.each do |t|
+    out += "<li><a href='/beanstalk_tube_stats?host=#{params[:host]}&tube=#{t}'>#{t}</a></li>"
+    out += ""
+  end
+  out += "</ul></font>"
+  out
+end
+
 
 ## HELPERS
 class String
@@ -83,7 +103,10 @@ class String
 end
 
 def beanstalk_stats(host, port, tube)
-  beanstalk = Beanstalk::Pool.new(["#{host}:#{port}"])
+  beanstalk = beanstalk_connection(host, port)
   beanstalk.stats_tube(tube)
-end    
+end
 
+def beanstalk_connection(host, port)
+  Beanstalk::Pool.new(["#{host}:#{port}"])
+end
